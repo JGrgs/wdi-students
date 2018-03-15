302c7472cfa158ac001fc8564aa5b4efb2d46c10
Mahima Rao <mahima.m23@gmail.com>
2018-03-14 17:00:45 -0700

    added more code with paths

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const app = express()
 const mongoose = require("mongoose")
 const logger = require("morgan")
 const bodyParser = require("body-parser")
+const Student = require("./models/Student.js")
 const PORT = 3000
 
 mongoose.connect("mongodb://localhost/wdi-students", (err) =>{
-    console.lo(err || "Connected to MONGO!!")
+    console.log(err || "Connected to MONGO!!")
+})
+
+app.use(logger("dev"))
+app.use(bodyParser.json())
+app.use(express.static(`__dirname()/public`))
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+app.get("/api/students", (req, res) => {
+    Student.find({}, (err, allDemStudents) => {
+        if(err) return console.log(err)
+        res.json(allDemStudents)
+    })
 })
 
 app.listen(PORT, (err) => {
